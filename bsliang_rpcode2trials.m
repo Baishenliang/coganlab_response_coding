edfd2d762a53769d629381d5495e7096c5335482
Baishenliang <liangbs95@gmail.com>
2024-10-04 20:46:42 -0400

    New version of rpcode2trials.m. Code events by conditions and response errors

--- bsliang_rpcode2trials.m
+++ bsliang_rpcode2trials.m
@@ -2,8 +2,8 @@
 clear all
 
 %% Locs
-Trial_loc='C:\Users\bl314\Box\CoganLab\D_Data\LexicalDecRepDelay\D103\240106\mat';
-RPcode_loc='C:\Users\bl314\Box\CoganLab\ECoG_Task_Data\response_coding\response_coding_results\LexicalDecRepDelay\D103';
+Trial_loc='C:\Users\bl314\Box\CoganLab\D_Data\LexicalDecRepDelay\D101\231022\mat';
+RPcode_loc='C:\Users\bl314\Box\CoganLab\ECoG_Task_Data\response_coding\response_coding_results\LexicalDecRepDelay\D101';
 
 %% load files
 load(fullfile(Trial_loc,"Trials.mat"));
@@ -33,6 +33,8 @@ load word_lst
 % add variables
 if length(ResponseStart)==length(Trials)
     for t=1:length(ResponseStart)
+
+        %% Temporal coding
         % Calculate the time difference between response coding and
         % temporal information from the recording.
         % Response coding time points are aligned to recorded sound files.
@@ -43,6 +45,8 @@ if length(ResponseStart)==length(Trials)
         Trials(t).StimCue = StimCue(t);
         Trials(t).ResponseStart = 30000*ResponseStart(t)-Diff_RScode_EDFcode;
         Trials(t).ResponseEnd = 30000*ResponseEnd(t)-Diff_RScode_EDFcode;
+
+        %% Condition coding
 
         % get the yes and no
         if isequal(trialInfo{1,t}.cue,'Yes/No')
@@ -61,97 +65,96 @@ if length(ResponseStart)==length(Trials)
             msgbox('Wrong word cue. Check the response coding.');
         end
 
-        Trials(t).Cue_Tag = ['Cue','/',Task_type_Tag,'/',Task_word_Tag];
-        Trials(t).Auditory_Tag = ['Auditory_stim','/',Task_type_Tag,'/',Task_word_Tag,'/',Trials(t).Stimcue];
-        Trials(t).Delay_Tag = ['Delay','/',Task_type_Tag,'/',Task_word_Tag];
+        %% Error, noise, and late response coding
+
+        % 1 ERR_TASK_YN_REP Task error: yes/no task but word repetition
+        % 2 ERR_TASK_REP_YN Task error: word repetition but yes/no task	
+        % 3 ERR_RESP_YN_YN Response error: yes/no task, should say yes, but say no	
+        % 4 ERR_RESP_YN_NY Response error: yes/no task, should say no, but say yes	
+        % 5 ERR_RESP_REP_WRO Response error: repetition task, say a totally wrong word/nonword
+        % 6 ERR_RESP_REP_MIS Response error: repetition task, say a word/nonword with mistakes at phonemic or syllabic levels
+        % 7 NOISY Noisy
+
+        % get the error type
+        Err_Tag = error_code.Var3(t);
+        Resp_Err = [];
+
+        if ~isempty(Err_Tag)
+            if contains(Err_Tag, 'ERR_TASK_YN_REP')
+                Resp_Err='ERR_TASK/YN_REP';
+            elseif contains(Err_Tag, 'ERR_TASK_REP_YN')
+                Resp_Err='ERR_TASK/REP_YN';
+            elseif contains(Err_Tag, 'ERR_RESP_REP_WRO')
+                try
+                    Resp_Err=['ERR_RESP/REP_WRO','/',Err_Tag{1}(18:end)];
+                catch
+                    Resp_Err='ERR_RESP/REP_WRO';
+                end
+            elseif contains(Err_Tag, 'ERR_RESP_REP_MIS')
+                try
+                    Resp_Err=['ERR_RESP/REP_MIS','/',Err_Tag{1}(18:end)];
+                catch
+                    Resp_Err='ERR_RESP/REP_MIS';
+                end
+            elseif contains(Err_Tag, 'NOISE')
+                Resp_Err='NOISE';
+            end
+        else
+            if isequal(trialInfo{1,t}.cue,'Yes/No')
+                % lexical task error coding
+                
+                % get response Y/N
+                resp_YN=response_code.Var3(t);
+    
+                % 3 ERR_RESP_YN_YN Response error: yes/no task, should say yes, but say no
+                if isequal(Task_word_Tag,'Word') && strcmpi(resp_YN,'no')
+                    Resp_Err='ERR_RESP/YN_YN';
+                end
+    
+                % 4 ERR_RESP_YN_NY Response error: yes/no task, should say no, but say yes
+                if isequal(Task_word_Tag,'Nonword') && strcmpi(resp_YN,'yes')
+                    Resp_Err='ERR_RESP/YN_NY';
+                end
+    
+            end
+        end
+
+        % If the patient responses too late (i.e., the response end comes later
+        % than the starting point of the next trial). Then mark all the events
+        % of the current trials as "LATE_RESP"
+        if t<length(ResponseStart) && Trials(t+1).Start-Trials(t).ResponseEnd<0
+            if isempty(Resp_Err)
+                Resp_Err='LATE_RESP';
+            else
+                Resp_Err=[Resp_Err,'/LATE_RESP'];
+            end
+        end
+        % For the trial before, if the patient responses too late and affect the baseline.
+        % Then mark all the event of the current trials as "NOISY_BSL"
+        if t>1 && Trials(t).Auditory-0.5-Trials(t-1).ResponseEnd<0
+            if isempty(Resp_Err)
+                Resp_Err='NOISY_BSL';
+            else
+                Resp_Err=[Resp_Err,'/NOISY_BSL'];
+            end
+        end
+    
+        % If Resp_Err is still empty, make it as "CORRECT".
+        if isempty(Resp_Err)
+            Resp_Err='CORRECT';
+        end
+    
+
+        %% Add the condition and error codings to Cue, Auditoru stimuli, Delay
+        Trials(t).Cue_Tag = ['Cue','/',Task_type_Tag,'/',Task_word_Tag,'/',Trials(t).StimCue{1},'/',Resp_Err];
+        Trials(t).Auditory_Tag = ['Auditory_stim','/',Task_type_Tag,'/',Task_word_Tag,'/',Trials(t).StimCue{1},'/',Resp_Err];
+        Trials(t).Delay_Tag = ['Delay','/',Task_type_Tag,'/',Task_word_Tag,'/',Trials(t).StimCue{1},'/',Resp_Err];
+        Trials(t).Response_Tag = ['Resp','/',Task_type_Tag,'/',Task_word_Tag,'/',Trials(t).StimCue{1},'/',Resp_Err];
+
+
     end
 else
     msgbox('Trial number does not match response coding!')
-end
-
-%% error codes
-%  also determines whether a yes/no trial is responded correctly
-%  (i.e., whether type 3 or type 3 error)
-
-% 1 ERR_TASK_YN_REP Task error: yes/no task but word repetition
-% 2 ERR_TASK_REP_YN Task error: word repetition but yes/no task	
-% 3 ERR_RESP_YN_YN Response error: yes/no task, should say yes, but say no	
-% 4 ERR_RESP_YN_NY Response error: yes/no task, should say no, but say yes	
-% 5 ERR_RESP_REP_WRO Response error: repetition task, say a totally wrong word/nonword
-% 6 ERR_RESP_REP_MIS Response error: repetition task, say a word/nonword with mistakes at phonemic or syllabic levels
-% 7 NOISY Noisy
-
-
-for t=1:length(ResponseStart)
-    Err_Tag = error_code.Var3(t);
-    if ~isempty(Err_Tag)
-        if contains(Err_Tag, 'ERR_TASK_YN_REP')
-            Trials(t).Response_Tag='ERR_TASK/YN_REP';
-        elseif contains(Err_Tag, 'ERR_TASK_REP_YN')
-            Trials(t).Response_Tag='ERR_TASK/REP_YN';
-        elseif contains(Err_Tag, 'ERR_RESP_REP_WRO')
-            try
-                Trials(t).Response_Tag=['ERR_RESP/REP_WRO',Err_Tag(17:end)];
-            catch
-                Trials(t).Response_Tag='ERR_RESP/REP_WRO';
-            end
-        elseif contains(Err_Tag, 'ERR_RESP_REP_MIS')
-            try
-                Trials(t).Response_Tag=['ERR_RESP/REP_MIS',Err_Tag(17:end)];
-            catch
-                Trials(t).Response_Tag='ERR_RESP/REP_MIS';
-            end
-        elseif contains(Err_Tag, 'NOISE')
-            Trials(t).Response_Tag='NOISE';
-        end
-    else
-        if isequal(trialInfo{1,t}.cue,'Yes/No')
-            % lexical task error coding
-
-            % get cue word and lexical property
-            cue_word=trialInfo{1,t}.sound;
-            if any(strcmp(cue_word, words))
-                lexical_tag=1; % is a word
-            elseif any(strcmp(cue_word, nonwords))
-                lexical_tag=2; % not a word
-            else
-                msgbox('Wrong word cue. Check the response coding.');
-            end
-            
-            % get response Y/N
-            resp_YN=response_code.Var3(t);
-
-            % 3 ERR_RESP_YN_YN Response error: yes/no task, should say yes, but say no
-            if lexical_tag==1 && strcmpi(resp_YN,'no')
-                %Trials(t).Response_Tag=3;
-                Trials(t).Response_Tag='ERR_RESP/YN_YN';
-            end
-
-            % 4 ERR_RESP_YN_NY Response error: yes/no task, should say no, but say yes
-            if lexical_tag==1 && strcmpi(resp_YN,'yes')
-                %Trials(t).Response_Tag=4;
-                Trials(t).Response_Tag='ERR_RESP/YN_NY';
-            end
-
-        end
-    end
-
-    % If it is still empty, add "CORRECT" in the error code to avoid null.
-    if isempty(Trials(t).Response_Tag)
-        Trials(t).Response_Tag='CORRECT';
-    end
-
-    % If the patient responses too late (i.e., the response end comes later
-    % than the starting point of the next trial baseline). Then mark all the events
-    % of the current trials as "XXX_NOISY_BSL", and the response in the
-    % previous trial as "XXX_LATE_RESP".
-    if t>1 && Trials(t).Auditory-0.5-Trials(t-1).ResponseEnd<0
-        Trials(t-1).Response_Tag=[Trials(t-1).Response_Tag,'/LATE_RESP'];
-        Trials(t).Cue_Tag = [Trials(t).Cue_Tag,'/NOISY_BSL'];
-        Trials(t).Auditory_Tag = [Trials(t).Auditory_Tag,'/NOISY_BSL'];
-        Trials(t).Delay_Tag = [Trials(t).Delay_Tag,'/NOISY_BSL'];
-        Trials(t).Response_Tag=[Trials(t).Response_Tag,'/NOISY_BSL'];
-    end
 end
 
 save(fullfile(Trial_loc,"Trials.mat"),'Trials');
