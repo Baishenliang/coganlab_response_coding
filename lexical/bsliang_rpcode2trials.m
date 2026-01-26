15469a959306c38729495384b67227fcd1625d53
Baishenliang <liangbs95@gmail.com>
2026-01-26 17:54:37 -0500

    updated

--- lexical/bsliang_rpcode2trials.m
+++ lexical/bsliang_rpcode2trials.m
@@ -1,7 +1,7 @@
 % Write response coding to Trials.mat for BIDs formating
 clear all; clc
 
-subjects_Tag = ["D140"];
+subjects_Tag = ["D127"];
 
 task_type='LexicalDecRepDelay\';
 %task_type='LexicalDecRepNoDelay\';
